65b556dd82ea0513a9a7eaf853e212654495e68b
Bruno Anhaia <hiroki.zx@gmail.com>
2019-11-02 23:21:49 -0300

    POO: prjDimas

---
+++ 4semestre/POO/prjDimas/src/fatec/poo/model/Instrutor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author bruno
+ */
+public class Instrutor extends Pessoa{
+    
+    private String Formacao;
+    private String AreaAtuacao;
+            
+    public Instrutor(String CPF, String Nome) {
+        super(CPF, Nome);
+    }
+
+    public void setFormacao(String Formacao) {
+        this.Formacao = Formacao;
+    }
+
+    public void setAreaAtuacao(String AreaAtuacao) {
+        this.AreaAtuacao = AreaAtuacao;
+    }
+}
